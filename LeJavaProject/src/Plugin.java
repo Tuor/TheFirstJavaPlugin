007a8c9def9ec6b64619ed2eb58300a10ac872e9
Tuor <chudi1223@gmail.com>
2015-12-19 20:09:52 -0500

    Anthony Testing

--- LeJavaProject/src/Plugin.java
+++ LeJavaProject/src/Plugin.java
@@ -1,6 +1,21 @@
 
-public class Plugin {
-int x = 5;
-
-int y = 7;
-}
+	import org.bukkit.plugin.java.*;
+	import org.bukkit.command.*;
+	//Anthony is so cool
+	public class Plugin extends JavaPlugin{
+	 
+	 
+	    @Override
+	    public void onEnable() {
+	     getLogger().info("First Plug In Init");
+	     
+	    }
+	   
+	    @Override
+	    public void onDisable() {
+	    }
+	    
+	    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+	  return false;
+	    }
+	}
